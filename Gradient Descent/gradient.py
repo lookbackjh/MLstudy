0c61da93370d998dfc29df0b0cefbbdbfe6a7c53
Corpse-is-Talking <jjjjunho1011@gmail.com>
2021-10-01 17:04:15 +0900

    2021-1001 for studying ml

--- Gradient Descent/gradient.py
+++ Gradient Descent/gradient.py
@@ -3,7 +3,7 @@ import random
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-def featurescale(X_train):
+def featurescale(X_train,X_test):
     ## before doing the gradient, the data must be feature scaled.., 
     ## feature이 여러가지일때, 특정 feature의 범위는 gradient 를 시행할때, 큰 영향을 줄수있다. i.e 키, 몸무게가 feature 인 경우.
     ## 따라서, feature의 크기를 어느정도 조절해주는 도구가 필요한데, 이게 feature Scaling이다.
@@ -11,16 +11,17 @@ def featurescale(X_train):
     ## feature scaling은 min-max scaling과 standard normal scaling이 있는데, 두가지 모두 자주 사용된다. 
     ## 두가지 방법 은 쓰임세가 살짝 다르긴한데, 대부분의 상황에서 통용될수 있는 min-max scaling을 사용하도록 하겠다.
     scalar=MinMaxScaler()
-    data_num,feat_num=X_train.shape[0],X_train.shape[1]
     scalar.fit(X_train)
     X_train=scalar.transform(X_train)
-    return X_train
+    X_test=scalar.transform(X_test)
+    ##주의!!!: testset의 Scaling은 train set 의 스케일링과 동일하게 진행.
+    return X_train,X_test
 def splitData(X,y):
     ## 검증을 위해 데이터 X를 train 데이터와 test data로 분리시킴, 통상적으로 약 7:3 언저리 의 비율로 가름
     X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
     return X_train,X_test,y_train,y_test
 
-def SquareLossfunction(X,y,theta, l2_reg=0.01):
+def SquareLossfunction(X,y,theta, l2_reg=0):
     ## Square Loss 
     ## X: n* d matrix  : n=number of data, d=number of feature
     ## Y: n*1 matrix: n= number of data
@@ -28,15 +29,18 @@ def SquareLossfunction(X,y,theta, l2_reg=0.01):
     ## loss =avg(X*thata.T-Y+ l2_reg*(L2norm(theta)))
     m=X.shape[0]
     loss_term=np.mean(np.square((np.dot(X,theta)-y)))
-    reg_term=np.linalg.norm(theta)*(2/m)
+    reg_term=np.linalg.norm(theta)*l2_reg
     loss=loss_term+reg_term
     return loss
-def computegrad(X,y,theta,l2_reg=0.01):
+def computegrad(X,y,theta,l2_reg=0):
+    ## X: n* d matrix  : n=number of data, d=number of feature
+    ## Y: n*1 matrix: n= number of data
     m=X.shape[0]
     temp=np.dot(X,theta)-y
     grad_term=(2.0/m)*(np.dot(X.T,temp))
-    return grad_term  
-def gradDescent(X,y,alpha=0.05,num_iter=1000,backtracking=True): 
+    reg_term=2*l2_reg*theta
+    return grad_term +reg_term 
+def gradDescent(X,y,alpha=0.1,num_iter=1000,backtracking=True): 
     ## the very basic gradient descent..
     ##things to consider.. initialization of theta, iteration..
     feat_num=X.shape[1]
@@ -44,9 +48,9 @@ def gradDescent(X,y,alpha=0.05,num_iter=1000,backtracking=True):
 
     theta_hist=np.zeros((num_iter+1,feat_num)) ## storing the historical data of theta
     loss_hist = np.zeros(num_iter+1) ## storing loss value to see if gradient is doing well..
-    theta_init=np.random.rand(feat_num)
+    theta_init=np.random.rand(feat_num) ## 통상적으로 0,1 사이에 랜덤하게 생성
     theta_hist[0,:]=theta_init
-    loss_hist[0]=SquareLossfunction(X,y,theta_hist[0,:])
+    loss_hist[0]=SquareLossfunction(X,y,theta_hist[0,:]) ##이니셜
     bactracknum=0
     ## initialize theta with random number between 0 and 1
     if backtracking:
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MLstudy
 for studying basics of machine learning
+
+
+## Gradient.py
+has functions that helps me to do spliting data, scaling the data, get the gradient, loss .. etc
+데이터 정규화, 나누기, 그래디언트구하기, 로스함수 구하는 함수들 모아놓은것
+
+## test.ipynb
+위함수들을 이용해 실제로 grad descent를 적용해본 것.
